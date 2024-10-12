05f1de66830dfc43e5e076066bc2a5551929ae1b
Vladyslav Markov <vladislav.u.markov@gmail.com>
2024-10-13 01:41:47 +0200

    replace compiler-explorer with godbolt from p00f

--- init.lua
+++ init.lua
@@ -1023,7 +1023,34 @@ require('lazy').setup({
       }
     end,
   },
-  { 'krady21/compiler-explorer.nvim' },
+  {
+    'p00f/godbolt.nvim',
+    config = function()
+      require('godbolt').setup {
+        languages = {
+          cpp = { compiler = 'g122', options = {} },
+          c = { compiler = 'cg122', options = {} },
+          rust = { compiler = 'r1650', options = {} },
+          -- any_additional_filetype = { compiler = ..., options = ... },
+        },
+        auto_cleanup = true, -- remove highlights and autocommands on buffer close
+        highlight = {
+          cursor = 'Visual', -- `cursor = false` to disable
+          -- values in this table can be:
+          -- 1. existing highlight group
+          -- 2. hex color string starting with #
+          static = { '#222222', '#333333', '#444444', '#555555', '#444444', '#333333' },
+          -- `static = false` to disable
+        },
+        -- `highlight = false` to disable highlights
+        quickfix = {
+          enable = false, -- whether to populate the quickfix list in case of errors
+          auto_open = false, -- whether to open the quickfix list in case of errors
+        },
+        url = 'https://godbolt.org',
+      }
+    end,
+  },
   {
     'aaronhallaert/advanced-git-search.nvim',
     cmd = { 'AdvancedGitSearch' },
